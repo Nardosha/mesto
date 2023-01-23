5f32b83283dc257571b8459bc26bc741a2312aab
natali <NataliDegtyreva@yandex.ru>
2023-01-23 22:16:52 +0300

    feat: add general method for all action buttons

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ const popupForm = document.querySelector('.popup__form')
 const buttonEdit = document.querySelector('.profile__edit-button')
 const buttonClosePopup = document.querySelector('.form__button-close')
 
+const setEventListeners = () => {
+    const actionButtons = document.querySelectorAll('[data-action]')
+    console.log(actionButtons)
+
+    actionButtons.forEach(btn => {
+        btn.addEventListener('click', actionHandler)
+    })
+}
+
+setEventListeners()
+
 const ACTION_TYPES = {
     EDIT: 'EDIT',
-    CLOSE: 'CLOSE'
+    CLOSE: 'CLOSE',
 }
 const actionHandler = (e) => {
-    console.log(e)
+    const actionType = e.target.dataset.action
+    if (actionType === ACTION_TYPES.EDIT) {
+        openPopup()
+        setFormFields()
+    }
+    if (actionType === ACTION_TYPES.EDIT) {
+        closePopup()
+    }
 }
 
 const openPopup = () => {
+    console.log('OPEN')
     popup.classList.add('popup_opened')
     popupForm.classList.add('popup_opened')
-    setFormFields()
 }
 
 const closePopup = () => {
@@ -44,7 +62,7 @@ const getProfileValues = (formFieldsMap) => {
         const fieldName = child.dataset.userField
         const profileFieldValue = child.textContent
 
-        if (!formFieldsMap.get(fieldName)) {
+        if (fieldName && !formFieldsMap.get(fieldName)) {
             formFieldsMap.set(fieldName, profileFieldValue)
         }
     })
@@ -56,16 +74,18 @@ const setFormFields = () => {
     const formFields = popupForm.querySelectorAll('.form__input')
 
     const formFieldsMap = getFormFields(formFields)
-    const filledFieldsFromProfile = getProfileValues(formFieldsMap)
+    const profileFields = getProfileValues(formFieldsMap)
 
     formFields.forEach(field => {
         const fieldName = field.dataset.userField
-        if (filledFieldsFromProfile.has(fieldName)) {
-            field.value = filledFieldsFromProfile.get(fieldName)
+        if (profileFields.has(fieldName)) {
+            field.value = profileFields.get(fieldName)
         }
     })
+
+    console.log(profileFields);
 }
 
-buttonEdit.addEventListener('click', openPopup)
+buttonEdit.addEventListener('click', actionHandler)
 buttonClosePopup.addEventListener('click', closePopup)
 
