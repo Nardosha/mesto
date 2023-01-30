c3d464e8278c76017b94552b91213cdd3220311a
natali <NataliDegtyreva@yandex.ru>
2023-01-30 21:34:18 +0300

    fix: fix styles and js!(((((((

--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,31 @@
+const profileName = document.querySelector('.profile__full-name')
+const profileDescription = document.querySelector('.profile__description')
 const editButton = document.querySelector('.profile__edit-button')
 const closeButton = document.querySelector('.popup__button-close')
-const submitButton = document.querySelector('.form__button-submit')
-const popupContainer = document.querySelector('.popup__container')
+const popup = document.querySelector('.popup')
 const popupForm = document.querySelector('.popup__form')
+const inputName = popupForm.querySelector('.form__input_field_user-full-name')
+const inputDescription = popupForm.querySelector('.form__input_field_user-description')
 
-const getProfileValues = () => {
-    const userProfile = document.querySelector('.profile__settings')
-    const formFieldsMap = new Map()
+const openPopup = () => {
+    popup.classList.add('popup_opened')
 
-    Array.from(userProfile.children).forEach(child => {
-        const fieldName = child.dataset.userField
-        const profileFieldValue = child.textContent
-
-        if (fieldName) {
-            formFieldsMap.set(fieldName, profileFieldValue)
-        }
-    })
-
-    return formFieldsMap
+    inputName.value = profileName.textContent
+    inputDescription.value = profileDescription.textContent
 }
 
-const togglePopup = () => {
-    const page = document.querySelector('.root')
-    const popup = document.querySelector('.popup')
-
-    popup.classList.toggle('popup_opened')
-    popupContainer.classList.toggle('popup_opened')
-    page.classList.toggle('scroll-lock')
-}
-
-const setFormFields = () => {
-    const formFields = popupForm.querySelectorAll('.form__input')
-    const profileFields = getProfileValues()
-
-    formFields.forEach(field => {
-        const fieldName = field.dataset.userField
-        if (profileFields.has(fieldName)) {
-            field.value = profileFields.get(fieldName)
-        }
-    })
-}
-
-const editButtonHandler = () => {
-    togglePopup()
-    setFormFields()
+const closePopup = () => {
+    popup.classList.remove('popup_opened')
 }
 
 const submit = (e) => {
     e.preventDefault()
 
-    const userProfile = document.querySelector('.profile__settings')
-    const formData = new FormData(popupForm)
-
-    Array.from(userProfile.children).forEach(child => {
-        const fieldName = child.dataset.userField
-        const formField = formData.get(fieldName)
-
-        if (fieldName && formData.get(fieldName)) {
-            child.textContent = formField.toString()
-        }
-    })
-
-    togglePopup()
+    profileName.textContent = inputName.value
+    profileDescription.textContent = inputDescription.value
+    closePopup()
 }
 
-editButton.addEventListener('click', editButtonHandler)
-closeButton.addEventListener('click', togglePopup)
-submitButton.addEventListener('click', submit)
+editButton.addEventListener('click', openPopup)
+closeButton.addEventListener('click', closePopup)
+popupForm.addEventListener('submit', submit)
