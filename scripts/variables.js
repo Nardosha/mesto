dcb61c61e6c1d051081e7bf66d9b18560b79f12f
natali <NataliDegtyreva@yandex.ru>
2023-03-09 18:45:15 +0300

    fix: separate listeners, fix after review

--- scripts/variables.js
+++ scripts/variables.js
@@ -4,9 +4,11 @@ const validationOptions = {
     formSelector: '.form',
     formInputsSelector: '.form__inputs',
     formInputSelector:'.form__input',
+    formInputClass:'form__input',
     formInputTypeErrorClass:'form__input_type_error',
     formInputErrorActiveClass: 'form__input-error_active',
     formButtonSubmitSelector: '.form__button-submit',
+    formButtonSubmitClass: 'form__button-submit',
     formButtonSubmitTypeDisabledClass: 'form__button-submit_type_disabled',
 }
 
