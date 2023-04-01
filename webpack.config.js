4c9ce5277e928517de16a1bd6d9d42817b949657
natali <NataliDegtyreva@yandex.ru>
2023-04-01 17:25:41 +0300

    fix: fix image src, add fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    devtool:  'source-map',
     entry: './scripts/index.js',
     output: {
         filename: '[name].[hash:5].js', // название файла с уникальным хэшом
-        path: path.join(__dirname, '/build'), // складываем "filename" в папку build
+        path: path.join(__dirname, 'build'), // складываем "filename" в папку build
         clean: true // удалять старые файлы пееред новым билдом
     },
     devServer: {
@@ -14,7 +15,9 @@ module.exports = {
             directory: path.join(__dirname, 'build')
         },
         compress: true,
-        port: 9000
+        port: 9000,
+        hot: true,
+        open: true
     },
     plugins: [
         new HtmlWebpackPlugin({template: './index.html'}), // HTML плагин с указанием адреса html шаблона
@@ -44,12 +47,19 @@ module.exports = {
                 }, 'postcss-loader'] //лоадер- css-loader
             },
             {//правило для assets:
-                test: /\.(png|jpg)$/,// для всех файлов с расширением .png и .jpg
+                test: /\.(png|jpg|svg)$/, // для всех файлов с расширением .png и .jpg
                 type: 'asset/resource', // лоадер - asset/resource
                 generator: {
-                    filename: 'img/[name].[hash:5][ext]' // положить все ассеты в папку img с названием [name] и хэшом и расширением [ext]
+                    filename: 'img/[name].[hash:5][ext]' // положить все картинки в папку img с названием [name] и хэшом и расширением [ext]
                 }
-            }
+            },
+            {// правило для шрифтов
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[hash:5][ext]' // положить все шрифты в папку fonts с названием [name] и хэшом и расширением [ext]
+                }
+            },
         ]
     }
 
